de7d28cc0ac81cb6e6e93ece62b31a2a5623272b
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-05-16 20:53:07 -0400

    edits

--- models/User.js
+++ models/User.js
@@ -1,153 +1,63 @@
-const router = require('express').Router();
-const { User, Post, Comment, Vote } = require('../../models');
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-// get all users
-router.get('/', (req, res) => {
-  User.findAll({
-    attributes: { exclude: ['password'] }
-  })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// create our User model
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
-router.get('/:id', (req, res) => {
-  User.findOne({
-    attributes: { exclude: ['password'] },
-    where: {
-      id: req.params.id
-    },
-    include: [
-      {
-        model: Post,
-        attributes: ['id', 'title', 'content', 'user_id']
-      },
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'user_id'],
-        include: {
-          model: Post,
-          attributes: ['title']
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
         }
-      },
-      {
-        model: Post,
-        attributes: ['title'],
-      }
-    ]
-  })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+    },
+    {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
 
-router.post('/', (req, res) => {
-  User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  })
-    .then(dbUserData => {
-      req.session.save(() => {
-        req.session.user_id = dbUserData.id;
-        req.session.username = dbUserData.username;
-        req.session.loggedIn = true;
-  
-        res.json(dbUserData);
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.post('/login', (req, res) => {
-  User.findOne({
-    where: {
-      email: req.body.email
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        }, 
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+      
     }
-  }).then(dbUserData => {
-    if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
-      return;
-    }
+);
 
-    const validPassword = dbUserData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
-  
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
-    });
-  });
-});
-
-router.post('/logout', (req, res) => {
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  }
-  else {
-    res.status(404).end();
-  }
-});
-
-router.put('/:id', (req, res) => {
-  User.update(req.body, {
-    individualHooks: true,
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.delete('/:id', (req, res) => {
-  User.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-module.exports = router;
+module.exports = User;
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,59 +1,58 @@
 const sequelize = require('../config/connection');
 const { User, Post } = require('../models');
 
-const userdata = [
-  {
-    username: 'jessie0',
-    email: 'jessie0@gmail.com',
-    password: 'password123'
-  },
-  {
-    username: 'honey1',
-    email: 'honey1@gmail.com',
-    password: 'password123'
-  },
-  {
-    username: 'tyler2',
-    email: 'tyler2@gmail.com',
-    password: 'password123'
-  },
-  {
-    username: 'fallon3',
-    email: 'fallon3@gmail.com',
-    password: 'password123'
-  },
-  {
-    username: 'cece4',
-    email: 'cece4@gmail.com',
-    password: 'password123'
-  },
-  {
-    username: 'brook5',
-    email: 'brook5@gmail.com',
-    password: 'password123'
-  },
-  {
-    username: 'trinity6',
-    email: 'trinity6@gmail.com',
-    password: 'password123'
-  },
-  {
-    username: 'miles7',
-    email: 'miles7@gmail.com',
-    password: 'password123'
-  },
-  {
-    username: 'kerry8',
-    email: 'kerry8@gmail.com',
-    password: 'password123'
-  },
-  {
-    username: 'juan9',
-    email: 'juan9@gmail.com',
-    password: 'password123'
-  }
+const userInfo = [
+    {
+        username: 'leahf21',
+        email: 'leahf21@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'racheljohn25',
+        email: 'rjohnson@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'walkerj75',
+        email: 'walkerj75@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'bobbyj32',
+        email: 'jeanbob@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'loladavid4',
+        email: 'lodavid@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'kahold45',
+        email: 'katehold@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'holkai33',
+        email: 'hollyk3@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'derrik295',
+        email: 'derrikmc2@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'car9213',
+        email: 'carlossmith@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'luis45',
+        email: 'luisday4@email.com',
+        password: 'password000'
+    },
 ];
-
-const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
+const seedUsers = () => User.bulkCreate(userInfo, {individualHooks: true});
 
 module.exports = seedUsers;
